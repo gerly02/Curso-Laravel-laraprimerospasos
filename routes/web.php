7de807fe89efbc4f1b4cbb3c1e5babde4fc05af1
Gerly02 <geraldineparra2816@gmail.com>
2024-02-22 16:56:23 -0500

    Proyecto esqueleto 2

--- routes/web.php
+++ routes/web.php
@@ -29,15 +29,29 @@ use Illuminate\Support\Facades\Route;
 
 // });
 
-Route::get('/custom', function () {
-    return view('custom');
+
+// Tercer ejemplo 
+
+// Route::get('/custom', function () {
+
+//     $msj="Mensaje desde el servidor *-*";
+
+//     return view('custom', ['msj' => $msj]);
+// });
+
+
+// cuarto ejemplo: Reto crear una view llamada contacto 
+
+Route::get('/contacto', function () {
+    return view('contacto');
+    // return view('contacto');
+
+}) -> name('contacto');
+
+Route::get('/first', function () {
+    return view('first');
 
 });
-
-
-
-
-
 
 
 Route::get('/dashboard', function () {
