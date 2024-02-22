ea7e3bebb85d1b15a1607c2475d8b6eb7952a0b1
Gerly02 <geraldineparra2816@gmail.com>
2024-02-22 17:00:56 -0500

    Proyecto esqueleto 2

---
+++ resources/views/first.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido1</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <br>
+    <a href="/contacto">Contacto</a>
+    <br>
+    <a href="http://localhost:7000/contacto">Contacto</a>
+    <br>
+    <a href="{{ route('contacto') }}"> Contacto (Ruta con nombre)</a>
+</body>
+</html>
